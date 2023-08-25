b9140175206c735af9f5167e85f397ba6f36ac09
2003chase <brendanstrayer@gmail.com>
2023-08-24 22:22:19 -0600

    Map book properties to GQL fields

--- src/entities/Book.ts
+++ src/entities/Book.ts
@@ -1,24 +1,32 @@
 import { Entity, OptionalProps, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Book {
   [OptionalProps]?: "createdAt" | "updatedAt" | "publisher";
 
+  @Field()
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: "date" })
   createdAt: Date = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 
+  @Field()
   @Property({ type: "text" })
   title!: string;
 
+  @Field()
   @Property({ type: "text" })
   author!: string;
 
+  @Field({ nullable: true })
   @Property({ type: "text", nullable: true })
   publisher?: string;
 }
