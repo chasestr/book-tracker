aa717d55736ff80d8d27dfc806b270609b6fba42
2003chase <brendanstrayer@gmail.com>
2023-09-17 19:39:15 -0600

    Update context to include req/res and add session interface

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,13 @@
 import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
+import { Session } from "express-session";
+declare module "express-session" {
+  interface Session {
+    userId: number;
+  }
+}
 
 export type MyContext = {
   emFork: EntityManager<IDatabaseDriver<Connection>>;
+  req: Express.Request & { session: Session };
+  res: Express.Response;
 };
