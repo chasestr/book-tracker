5182605e3beb508a895716c9930421a7af829ea6
2003chase <brendanstrayer@gmail.com>
2023-12-04 14:12:18 -0700

    Display navbar across entire app

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import theme from "../theme";
+import { Navbar } from "../components/Navbar";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Navbar />
       <Component {...pageProps} />
     </ChakraProvider>
   );
 }
 
-export default MyApp;
+export default withUrqlClient(createUrqlClient)(MyApp);
