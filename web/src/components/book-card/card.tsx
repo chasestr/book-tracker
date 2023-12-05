ffa0f7fca34152278c53d0b62018ab5d670c1483
2003chase <brendanstrayer@gmail.com>
2023-12-04 21:03:35 -0700

    Add update/delete functionality to card (WIP)

--- web/src/components/book-card/card.tsx
+++ web/src/components/book-card/card.tsx
@@ -1,18 +1,43 @@
-import { Book } from "../../generated/graphql";
+import { IconButton, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { Book, useDeleteBookMutation } from "../../generated/graphql";
 import styles from "./card.module.css";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { useRouter } from "next/router";
 
 type BookCardProps = {
   book: Book;
 };
 
 export const BookCard: React.FC<BookCardProps> = (p) => {
+  const [, deleteBook] = useDeleteBookMutation();
+  const router = useRouter();
+
   return (
     <div className={styles.card}>
       <div className="card-body">
-        <h2>{p.book.title?.slice(0, 50)}</h2>
+        <NextLink href="/book/[id]" as={`/book/${p.book.id}`}>
+          <Link>
+            <h2>{p.book.title}</h2>
+          </Link>
+        </NextLink>
         <p>{p.book.author?.slice(0, 50)}</p>
         {p.book.genre ? <p>Genre: {p.book.genre.slice(0, 50)}</p> : null}
         {p.book.rating ? <p>Rating: {p.book.rating}</p> : null}
+        <IconButton
+          icon={<DeleteIcon color="red" />}
+          aria-label="Delete book"
+          onClick={() => {
+            deleteBook({ id: p.book.id });
+          }}
+        />
+        <IconButton
+          icon={<EditIcon />}
+          aria-label="Edit book"
+          onClick={() => {
+            router.push(`/book/edit/[${p.book.id}]`);
+          }}
+        />
       </div>
     </div>
   );
